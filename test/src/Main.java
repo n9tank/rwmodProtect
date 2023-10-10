83ae7a45c190ddaabeebb9120dcbb2418ecdbcbf
n9tank <n9tank@163.com>
2023-10-10 11:59:01 +0800

    0

--- test/src/Main.java
+++ test/src/Main.java
@@ -15,79 +15,87 @@ public class Main {
  static cmpU cm;
  public static void srot(ArrayList crr, int type) {
   cmpU cmp=cm;
-  cmp.type=type;
-  Collections.sort(crr,cmp);
+  cmp.type = type;
+  Collections.sort(crr, cmp);
  }
  public static void charList() throws Exception {
   char to=Character.MAX_VALUE;
   int index=to - 32;
   ArrayList crr=new ArrayList();
   StringBuilder dump=new StringBuilder();
-  Bitmap map=Bitmap.createBitmap(100,100,Bitmap.Config.ALPHA_8);
+  Bitmap map=Bitmap.createBitmap(100, 100, Bitmap.Config.ALPHA_8);
   Canvas vs=new Canvas(map);
   Paint pain=new Paint();
-  charc.paint=pain;
-  charc.recs=new Rect();
+  Rect rec=new Rect();
   pain.setAntiAlias(true);
   pain.setTextSize(40);
   pain.setColor(Color.BLACK);
-  charc.bit=map;
-  charc.pixe=new int[10000];
-  while (--index >= 0){
-   if(to>=0xd800&&to<=0xdfff){
-    to=0xd7ff;
-    index=to-32;
+  int pixes[]=new int[10000];
+  while (--index >= 0) {
+   if (to >= 0xd800 && to <= 0xdfff) {
+    to = 0xd7ff;
+    index = to - 32;
    }
    String s=String.valueOf(to--);
-   if (pain.hasGlyph(s)){
-    vs.drawText(s,50,50,pain);
-    charc c=new charc(s);
-    if(c.x<=0.f){
-    crr.add(c);
+   if (pain.hasGlyph(s)) {
+    float x=pain.measureText(s);
+    if (x <= 0.f) {
+     pain.getTextBounds(s, 0, 1, rec);
+     vs.drawText(s, -rec.left, -rec.top, pain);
+     int w=rec.width();
+     int h=rec.height();
+     map.getPixels(pixes, 0, w, 0, 0,w,h);
+     int ym=w*h;
+     int pi=0;
+     while (--ym >= 0) {
+      if (pixes[ym] != 0) {
+       ++pi;
+      }
+     }
+     charc c=new charc(s,x,rec.height(),pi);
+     crr.add(c);
+     map.eraseColor(0);
     }
-    map.eraseColor(0);
-   } else{
-   dump.append(s);
-   //dump.append("\n");
+   } else {
+    dump.append(s);
    }
   }
-  cm=new cmpU();
+  cm = new cmpU();
   int size=crr.size();
- // srot(crr,2);
-  srot(crr,0);
+  srot(crr, 0);
   size*=0.2;
   BufferedWriter buff=new BufferedWriter(new FileWriter("sdcard/a.txt"));
   int i=0;
   do{
-  charc c=(charc)crr.get(i);
-  buff.write(c.s);
-  }while(++i<size);
+   charc c=(charc)crr.get(i);
+   buff.write(c.s);
+  }while(++i < size);
   buff.write(dump.toString());
   buff.flush();
   buff.close();
  }
- public static void main(String arg[]) throws Exception{
+ public static void main(String arg[]) throws Exception {
   //toLib("sdcard/rustedWarfare/lib.apk");
   charList();
   System.out.print("finsh");
  }
  /*
- public static void toLib(String str) throws Exception{
+  public static void toLib(String str) throws Exception{
   ZipFile zip=new ZipFile(str);
   ZipOutputStream out=new ZipOutputStream(new BufferedOutputStream(new FileOutputStream("sdcard/lib.zip")));
   OutputStreamWriter wt=new OutputStreamWriter(out);
   Enumeration<? extends ZipEntry> en=zip.entries();
   while(en.hasMoreElements()){
-   ZipEntry zipe=en.nextElement();
-   String name;
-   if(!zipe.isDirectory()&&(name=zipe.getName()).endsWith(".ini")){
-    loderLib loder=new rust.loderLib(zip.getInputStream(zipe));
-    out.putNextEntry(new ZipEntry(name.substring(13)));
-    loder.write(wt);
-    out.closeEntry();
-   }
+  ZipEntry zipe=en.nextElement();
+  String name;
+  if(!zipe.isDirectory()&&(name=zipe.getName()).endsWith(".ini")){
+  loderLib loder=new rust.loderLib(zip.getInputStream(zipe));
+  out.putNextEntry(new ZipEntry(name.substring(13)));
+  loder.write(wt);
+  out.closeEntry();
+  }
   }
   zip.close();
   out.close();
- }*/
+  }*/
 }
--- test/src/charc.java
+++ test/src/charc.java
@@ -8,26 +8,10 @@ public int pix;
 public float x;
 public float y;
 public String s;
-public static Paint paint;
-public static Rect recs;
-public static Bitmap bit;
-public static int pixe[];
-public charc(String c){
+public charc(String c,float xm,int ym,int pi){
 s=c;
-Paint pa=paint;
-Rect rec=recs;
-pa.getTextBounds(s,0,1,rec);
-x=pa.measureText(s);
-y=rec.bottom-rec.top;
-int pi=0;
-int[] pixes=pixe;
-bit.getPixels(pixes,0,100,0,0,100,100);
-int y=10000;
-while (--y>=0) {
-if(pixes[y]!=0) {
-pi++;
-}
-}
+x=xm;
+y=ym;
 pix=pi;
 }
 }
