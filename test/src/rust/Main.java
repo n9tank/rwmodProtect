55dae2abc7045b11cd756c973318df4af0c9b1c6
n9tank <n9tank@163.com>
2023-10-13 14:51:27 +0800

    0

---
+++ test/src/rust/Main.java
@@ -0,0 +1,110 @@
+package rust;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
+
+
+public class Main {
+ static cmpU cm;
+ public static void srot(ArrayList crr, int type) {
+  cmpU cmp=cm;
+  cmp.type = type;
+  Collections.sort(crr, cmp);
+ }
+ public static void charList() throws Exception {
+  char to=Character.MAX_VALUE;
+  int index=to - 32;
+  ArrayList crr=new ArrayList();
+  StringBuilder dump=new StringBuilder();
+  Bitmap map=Bitmap.createBitmap(100, 100, Bitmap.Config.ALPHA_8);
+  Canvas vs=new Canvas(map);
+  Paint pain=new Paint();
+  Rect rec=new Rect();
+  pain.setAntiAlias(true);
+  pain.setTextSize(40);
+  pain.setColor(Color.BLACK);
+  int pixes[]=new int[10000];
+  while (--index >= 0) {
+   if (to >= 0xd800 && to <= 0xdfff) {
+    to = 0xd7ff;
+    index = to - 32;
+   }
+   String s=String.valueOf(to--);
+   if (pain.hasGlyph(s)) {
+    float x=pain.measureText(s);
+    if (x <= 0.f) {
+     pain.getTextBounds(s, 0, 1, rec);
+     vs.drawText(s, -rec.left, -rec.top, pain);
+     int w=rec.width();
+     int h=rec.height();
+     map.getPixels(pixes, 0, w, 0, 0, w, h);
+     int ym=w * h;
+     int pi=0;
+     while (--ym >= 0) {
+      if (pixes[ym] != 0) {
+       ++pi;
+      }
+     }
+     charc c=new charc(s, w, h, pi);
+     crr.add(c);
+     map.eraseColor(0);
+    }
+   } else {
+    dump.append(s);
+   }
+  }
+  cm = new cmpU();
+  int size=crr.size();
+  srot(crr, 0);
+  size *= 0.2;
+  BufferedWriter buff=new BufferedWriter(new FileWriter("sdcard/a.txt"));
+  int i=0;
+  do{
+   charc c=(charc)crr.get(i);
+   buff.write(c.s);
+  }while(++i < size);
+  buff.write(dump.toString());
+  buff.flush();
+  buff.close();
+ }
+ public static void main(String arg[]) throws Exception {
+  rwmodProtect.init(new File("/storage/emulated/0/AppProjects/j/run/.ini"),new ui("def"));
+  toLib("sdcard/rustedWarfare/lib.apk");
+  System.out.print("finsh");
+ }
+ public static void toLib(String str) throws Exception {
+  ZipFile zip=new ZipFile(str);
+  ZipOutputStream out=new ZipOutputStream(new BufferedOutputStream(new FileOutputStream("sdcard/lib.zip")));
+  out.setLevel(9);
+  OutputStreamWriter wt=new OutputStreamWriter(out);
+  Enumeration<? extends ZipEntry> en=zip.entries();
+  StringBuilder def= new StringBuilder();
+  while (en.hasMoreElements()) {
+   ZipEntry zipe=en.nextElement();
+   String name;
+   if (!zipe.isDirectory() && (name = zipe.getName()).endsWith(".ini")) {
+    loder loder=new loder(new InputStreamReader(zip.getInputStream(zipe)), def);
+    out.putNextEntry(new ZipEntry(name.substring(13)));
+    loder.write(wt);
+    out.closeEntry();
+   }
+  }
+  zip.close();
+  out.close();
+ }
+}
---
+++ test/src/rust/cmpU.java
@@ -0,0 +1,27 @@
+package rust;
+import java.util.Comparator;
+public class cmpU implements Comparator<charc> {
+ int type;
+ public int compare(charc o1, charc o2) {
+  int m,n;
+  switch(type) {
+   case 0:
+    m = o1.pix;
+    n = o2.pix;
+    break;
+   case 1:
+    m = o1.x;
+    n = o2.x;
+    break;
+   case 2:
+    m = o1.y;
+    n = o2.y;
+    break;
+   default:
+    m = (Math.max(o1.x, 1) * Math.max(o1.y,1));
+    n = (Math.max(o2.x, 1) * Math.max(o2.y,1));
+    break;
+  }
+  return m-n;
+ }
+}
